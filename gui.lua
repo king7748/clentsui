72a49b90deda8e9d307365b205367bc20868b080
king7748 <132284562+king7748@users.noreply.github.com>
2023-11-30 10:22:12 +0300

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,51 +1,8 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Neptune X", HidePremium = false, SaveConfig = true, ConfigFolder = "Config", IntroEnabled = true, IntroText = Neptune X})
+local Window = OrionLib:MakeWindow({Name = "Neptune X", HidePremium = false, SaveConfig = true, ConfigFolder = "Config", IntroEnabled = true, IntroText = "Neptune X"})
 
-local Tab = Window:MakeTab({
-	Name = "Tab 1",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
 
---[[
-Name = <string> - The name of the tab.
-Icon = <string> - The icon of the tab.
-PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
-]]
 
-local Section = Tab:AddSection({
-	Name = "Player"
-})
-
---[[
-Name = <string> - The name of the section.
-]]
-
-OrionLib:MakeNotification({
-	Name = "Title!",
-	Content = "Notification content... what will it say??",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-})
-
---[[
-Title = <string> - The title of the notification.
-Content = <string> - The content of the notification.
-Image = <string> - The icon of the notification.
-Time = <number> - The duration of the notfication.
-]]
-
-Tab:AddButton({
-	Name = "Icehub",
-	Callback = loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMael7/NewIceHub/main/Brookhaven"))()()
-      		print("button pressed")
-  	end    
-})
-
---[[
-Name = <string> - The name of the button.
-Callback = <function> - The function of the button.
-]]
 
 end
 OrionLib:Init()
