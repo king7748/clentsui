332b29db60db9435f1678029c5cea56db3c254dc
king7748 <132284562+king7748@users.noreply.github.com>
2023-11-30 10:28:40 +0300

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Neptune X", HidePremium = false, SaveConfig = true, ConfigFolder = "Config", IntroEnabled = true, IntroText = "Neptune X"})
+local Window = OrionLib:MakeWindow({Name = "Neptune X", HidePremium = false, SaveConfig = true, ConfigFolder = "Config", IntroEnabled = true, IntroText = "Neptune X", Icon = "https://ibb.co/pKnTcMP"})
 
 
 
