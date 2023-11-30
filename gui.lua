ac8c436892af7ed621d13cc8c8b8748d5ae2f9da
king7748 <132284562+king7748@users.noreply.github.com>
2023-11-30 10:52:43 +0300

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,8 +1,41 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Neptune X", HidePremium = false, SaveConfig = true, ConfigFolder = "Config", IntroEnabled = true, IntroText = "Neptune X", Icon = "https://ibb.co/pKnTcMP"})
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
+local Window = Rayfield:CreateWindow({
+   Name = "Neptune X",
+   LoadingTitle = "Neptune X",
+   LoadingSubtitle = "by clent",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
 
+local Tab = Window:CreateTab("Player", 4483362458) -- Title, Image
 
-
-end
-OrionLib:Init()
+local Slider = Tab:CreateSlider({
+   Name = "Speed",
+   Range = {10, 100},
+   Increment = 10,
+   Suffix = "Bananas",
+   CurrentValue = 10,
+   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   game.Players.LocalPlayer Character:SetAttribute("SpeedMultiplier", Value)
+   end,
+})
